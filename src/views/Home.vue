4c6cfb44c0d7911ff454765645deb1b1b5f458a8
benji <benjisp018@gmail.com>
2019-10-06 23:38:40 -0400

    adds movie app front end

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,100 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>New Actor</h1>
+    <div>
+      First Name:
+      <input type="text" v-model="newActorFirstName" />
+      Last Name:
+      <input type="text" v-model="newActorLastName" />
+      Gender:
+      <input type="text" v-model="newActorGender" />
+      Age:
+      <input type="text" v-model="newActorAge" />
+      <button v-on:click="createActor()">Create Actor</button>
+    </div>
+    <h1>All Actors</h1>
+    <div v-for="actor in actors">
+      <h2>{{ actor.first_name }} {{ actor.last_name }}</h2>
+      <button v-on:click="showActor(actor)">Show more</button>
+      <div v-if="currentActor === actor">
+        <p>Gender: {{ actor.gender }}</p>
+        <p>Age: {{ actor.age }}</p>
+        <div>
+          First Name:
+          <input type="text" v-model="actor.first_name" />
+          Last Name:
+          <input type="text" v-model="actor.last_name" />
+          Gender:
+          <input type="text" v-model="actor.gender" />
+          Age:
+          <input type="text" v-model="actor.age" />
+          <button v-on:click="updateActor(actor)">Update Actor</button>
+          <button v-on:click="destroyActor(actor)">Destroy Actor</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      actors: [],
+      currentActor: {},
+      newActorFirstName: "",
+      newActorLastName: "",
+      newActorGender: "",
+      newActorAge: ""
+    };
+  },
+  created: function() {
+    axios.get("/api/actors").then(response => {
+      this.actors = response.data;
+    });
+  },
+  methods: {
+    createActor: function() {
+      var params = {
+        first_name: this.newActorFirstName,
+        last_name: this.newActorLastName,
+        gender: this.newActorGender,
+        age: this.newActorAge
+      };
+      axios.post("/api/actors", params).then(response => {
+        this.actors.push(response.data);
+        this.newActorFirstName = "";
+        this.newActorLastName = "";
+        this.newActorGender = "";
+        this.newActorAge = "";
+      });
+    },
+    showActor: function(actor) {
+      if (this.currentActor === actor) {
+        this.currentActor = {};
+      } else {
+        this.currentActor = actor;
+      }
+    },
+    updateActor: function(actor) {
+      var params = {
+        first_name: actor.first_name,
+        last_name: actor.last_name,
+        gender: actor.gender,
+        age: actor.age
+      };
+      axios.patch("/api/actors/" + actor.id, params).then(response => {
+        this.currentActor = {};
+      });
+    },
+    destroyActor: function(actor) {
+      axios.delete("/api/actors/" + actor.id).then(response => {
+        var index = this.actors.indexOf(actor);
+        this.actors.splice(index, 1);
+      });
+    }
   }
-}
+};
 </script>
